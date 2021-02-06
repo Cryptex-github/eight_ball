8fcfb30fb0246bb4d94a3026326db6d36a29ad8e
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-06 12:30:36 -0800

    format

--- async8ball/__init__.py
+++ async8ball/__init__.py
@@ -12,21 +12,44 @@ Here we start the ball you can pass in your own asyncio event loop if you wish
 class ball:
  def __init__(self, loop=None):
   self.loop = asyncio.get_event_loop() if loop is None else loop
-  self.responses_list=["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes – definitely.", "You may rely on it."]
+  self.responses_list=["As I see it, yes.", 
+                       "Ask again later.", 
+                       "Better not tell you now.", 
+                       "Cannot predict now.", 
+                       "Concentrate and ask again.", 
+                       "Don’t count on it.", 
+                       "It is certain.", 
+                       "It is decidedly so.", 
+                       "Most likely.", 
+                       "My reply is no.", 
+                       "My sources say no.", 
+                       "Outlook not so good.", 
+                       "Outlook good.", 
+                       "Reply hazy, try again.", 
+                       "Signs point to yes.", 
+                       "Very doubtful.", 
+                       "Without a doubt.", 
+                       "Yes.", 
+                       "Yes – definitely.", 
+                       "You may rely on it."]
  async def add_response_from_file(self, filename):
   async with AIOFile(filename, "r") as f:
    choices = await f.read().split("\n")
    for i in choices:
     self.responses_list.append(i)
+    
  async def override_response_from_file(self, filename):
   async with AIOFile(filename, "r") as f:
    choices = await f.read().split("\n")
    self.responses_list=choices
+   
  async def add_response(self, response):
   self.responses_list.append(response)
+  
  async def add_response_from_list(self, response_list):
   for i in response_list:
    self.response_list.append(i)
- async def response(self, question) -> str:
+   
+ async def response(self, question):
    random.seed(len(question)*random.randint(0,10))
    return random.choice(self.responses_list)
