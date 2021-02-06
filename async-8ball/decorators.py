321eca21e316bfd026ec56fbd979c8dd671eb184
Con <50535379+ConnorTippets@users.noreply.github.com>
2021-02-05 21:13:05 -0600

    Update list of choices

--- async-8ball/decorators.py
+++ async-8ball/decorators.py
@@ -1,6 +1,24 @@
 import random
 import functools
-choices = ['Yes', 'No', 'Yes']
+choices = ["It is certain","Without a doubt", "\
+You may rely on it","\
+Yes definitely","\
+It is decidedly so","\
+As I see it, yes","\
+Most likely","\
+Yes","\
+Outlook good","\
+Signs point to yes","\
+Reply hazy try again","\
+Better not tell you now","\
+Ask again later","\
+Cannot predict now","\
+Concentrate and ask again","\
+Don’t count on it","\
+Outlook not so good","\
+My sources say no","\
+Very doubtful","\
+My reply is no"]
 
 def ball_decorator(func):
   @functools.wraps(func)
