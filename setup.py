ad5e972e2606c461ea009fff5a61476aa4933649
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-05 20:39:26 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='async-8ball',
     version='1.0',
     description='Asynchronous 8ball response generator.',
-    long_description_content_type=text/markdown,
+    long_description_content_type="text/markdown",
     long_description=long_description,
     url='https://github.com/Cryptex-github/async-8ball',
     author='cryptex',
