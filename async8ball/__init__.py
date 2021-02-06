16422912d36dc3c43478093184688d66508ab3df
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-05 21:00:35 -0800

    Add files via upload

---
+++ async8ball/__init__.py
@@ -0,0 +1,9 @@
+import random
+
+responses_list=["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don’t count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes – definitely.", "You may rely on it."]
+
+__all__ = "__version__"
+__version__ = "0.1"
+
+async def response(self) -> str:
+  return random.choice(responses_list)
