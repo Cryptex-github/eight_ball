9b1e2da417d73cf9edc27fa7fa3847d0818e1469
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-06 15:21:53 -0800

    sync

--- eight_ball/__init__.py
+++ eight_ball/__init__.py
@@ -1,17 +1,14 @@
 import random
-import asyncio
-from aiofile import AIOFile
 
 __all__ = ("__version__", "ball")
-__version__ = "2.0"
+__version__ = "3.0"
 class ballException(Exception):
  pass
 """
-Here we start the ball you can pass in your own asyncio event loop if you wish
+Here we start the ball
 """
 class ball:
- def __init__(self, loop=None):
-  self.loop = asyncio.get_event_loop() if loop is None else loop
+ def __init__(self):
   self.responses_list=["As I see it, yes.", 
                        "Ask again later.", 
                        "Better not tell you now.", 
@@ -34,24 +31,24 @@ class ball:
                        "You may rely on it.",
                        "What do you think?",
                        "Maybe yes maybe not"]
- async def add_response_from_file(self, filename):
-  async with AIOFile(filename, "r") as f:
-   choices = await f.read().split("\n")
+ def add_response_from_file(self, filename):
+  with open(filename, "r") as f:
+   choices = f.read().split("\n")
    for i in choices:
     self.responses_list.append(i)
     
- async def override_response_from_file(self, filename):
-  async with AIOFile(filename, "r") as f:
-   choices = await f.read().split("\n")
+ def override_response_from_file(self, filename):
+  with open(filename, "r") as f:
+   choices = f.read().split("\n")
    self.responses_list=choices
    
- async def add_response(self, response):
+ def add_response(self, response):
   self.responses_list.append(response)
   
- async def add_response_from_list(self, response_list):
+ def add_response_from_list(self, response_list):
   for i in response_list:
    self.response_list.append(i)
    
- async def response(self, question):
+ def response(self, question):
    random.seed(len(question)*random.randint(0,10))
    return random.choice(self.responses_list)
