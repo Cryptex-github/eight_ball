92bf5f9b0750febd2ba92676c632a3cad14d5a2c
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-02-05 20:47:21 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(
-    name='async-8ball',
-    version='1.0',
+    name='async_8ball',
+    version='1.1',
     description='Asynchronous 8ball response generator.',
     long_description_content_type="text/markdown",
     long_description=long_description,
-    url='https://github.com/Cryptex-github/async-8ball',
+    url='https://github.com/Cryptex-github/async_8ball',
     author='cryptex',
     license='Apache-2.0',
-    packages=["async-8ball"],
+    packages=["async_8ball"],
     zip_safe=False,
 )
